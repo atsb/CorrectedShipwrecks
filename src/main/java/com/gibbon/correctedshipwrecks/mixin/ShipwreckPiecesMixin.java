e1100796f0e287289684c596e3be746421b6ea75
atsb <44937323+atsb@users.noreply.github.com>
2026-07-30 18:17:39 +0200

    Full Release version (server side)

--- src/main/java/com/gibbon/correctedshipwrecks/mixin/ShipwreckPiecesMixin.java
+++ src/main/java/com/gibbon/correctedshipwrecks/mixin/ShipwreckPiecesMixin.java
@@ -1,25 +1,3 @@
-/*
- * Copyright (c) 2026 Gibbon
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACTBinding, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 package com.gibbon.correctedshipwrecks.mixin;
 
 import net.minecraft.core.BlockPos;
@@ -46,45 +24,44 @@ public abstract class ShipwreckPiecesMixin extends StructurePiece {
                     target = "Lnet/minecraft/world/level/WorldGenLevel;getHeight(Lnet/minecraft/world/level/levelgen/Heightmap$Types;II)I"
             )
     )
-
-    // method that fixes the calculations based on WORLD_SURFACE_WG and OCEAN_FLOOR_WG.
     private int filterShipwreckGroundHeight(WorldGenLevel level, Heightmap.Types heightmapType, int x, int z) {
-        if (heightmapType == Heightmap.Types.WORLD_SURFACE_WG) {
-            int oceanFloorY = level.getHeight(Heightmap.Types.OCEAN_FLOOR_WG, x, z);
-            BlockPos.MutableBlockPos pos = new BlockPos.MutableBlockPos(x, oceanFloorY, z);
-            int targetY = level.getSeaLevel();
+        int vanillaY = level.getHeight(heightmapType, x, z);
+        BlockPos.MutableBlockPos pos = new BlockPos.MutableBlockPos(x, vanillaY, z);
 
-            while (pos.getY() > level.getSeaLevel()) {
-                BlockState blockState = level.getBlockState(pos);
-
-                if (isSolidGround(blockState)) {
-                    targetY = pos.getY();
-                    break;
+        // find the actual highest / topmost block
+        while (pos.getY() > level.getMinY() && level.getBlockState(pos).isAir()) {
+            pos.move(0, -1, 0);
+        }
+        BlockState topState = level.getBlockState(pos);
+        if (isIceOrSnow(topState)) {
+            // sink the ship down to the ocean floor
+            while (pos.getY() > level.getMinY()) {
+                BlockState state = level.getBlockState(pos);
+                if (!state.isAir() && !isIceOrSnow(state) && !isWaterOrPlant(state)) {
+                    // found the actual seabed
+                    return pos.getY();
                 }
                 pos.move(0, -1, 0);
             }
-
-	    // we embed the wrecks into the seabed / floor a little bit
-            return Math.max(level.getMinY(), targetY - 3);
         }
-        return level.getHeight(heightmapType, x, z);
+
+        // if it's not ice we return the vanilla height.
+        return vanillaY;
     }
 
-    // avoid all we can that pertains to wrecks.  This allows generation into seabeds full of life and allows the generation logic to also place seagrass etc onto the wreck.
-    private boolean isSolidGround(BlockState blockState) {
-        if (blockState.isAir()) {
-            return true;
-        }
-
+    private boolean isIceOrSnow(BlockState blockState) {
         String blockId = blockState.getBlock().getDescriptionId().toLowerCase();
-        boolean isNonSolidBlock = blockId.contains("cloud")
-                || blockId.contains("packed_ice")
+        return blockId.contains("packed_ice")
                 || blockId.contains("blue_ice")
                 || blockId.contains("ice")
-                || blockId.contains("water")
+                || blockId.contains("snow");
+    }
+
+    private boolean isWaterOrPlant(BlockState blockState) {
+        String blockId = blockState.getBlock().getDescriptionId().toLowerCase();
+        return blockId.contains("water")
                 || blockId.contains("kelp")
                 || blockId.contains("seagrass")
                 || blockId.contains("pickle");
-        return !isNonSolidBlock;
     }
 }
